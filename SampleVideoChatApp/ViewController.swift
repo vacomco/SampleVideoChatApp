a81dfed8251e6aa85a5c1a93101ab16b9a9e1789
wdev <wdev@local>
2023-07-01 17:32:33 +1000

    Initial Commit

---
+++ SampleVideoChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleVideoChatApp
+//
+//  Created by Wen Chen on 2023/7/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
